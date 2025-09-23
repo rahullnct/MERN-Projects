5415ee373ffce2fb5e0560ea505c9f84abd19f7f
rahullnct <rahulyadavkv01@gmail.com>
2025-09-23 01:51:52 -1200

    today night changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
 import viteLogo from '/vite.svg'
 import './App.css'
 // import NewComponents from './Components/FirstComponent';
-import { Routes,Route } from 'react-router-dom';
-import Home from '../src/Pages/Home';
-import About from '../src/Pages/About';
-import Contact from '../src/Pages/Contact';
-import Doctor from '../src/Pages/Doctor';
-import Signup from '../src/Authentication/Signup';
-import Login from '../src/Authentication/Login';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Doctor from './Pages/Doctor';
+import Signup from './Authentication/Signup';
+import Login from './Authentication/Login';
 import NewComponent from './Components/NewComponent';
 function App() {
   return (
-  <div className='App'>
+  <div className='main_wrapper'>
   <Routes>
-    <Route path='/' element={<NewComponent/>}>
-    <Route index element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/contact' element={<Contact/>}/>
-    <Route path='/doctor' element={<Doctor/>}/>
-    <Route path='/signup' element={<Signup/>}/>
-    <Route path='/login' element={<Login/>}/>
-    
-    
-    </Route>
+  <Route path="/" element={<NewComponent />}>
+  <Route index element={<Home />} />
+  <Route path="about" element={<About />}/>
+  <Route path="contact" element={<Contact />}/>
+  <Route path="doctor" element={<Doctor />} />
+  <Route path="signup" element={<Signup />} />
+  <Route path="login" element={<Login />} />
+  </Route>
   </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,41 @@
-function Home(){
-    return(
-        <div>
-            Home Page
-        </div>
-    )
-};
+import { useNavigate } from "react-router-dom"; 
+import { assets,specialityData} from "../assets/assets.js";
+ import '../CSS_Folders/Home.css';
+  function Home(){
+     const navigate=useNavigate(); 
+     return( 
+     <div className="home_wrapper">
+         <div className="home_container">
+             <div className="main_part">
+                 <div className="headings_subheadings">
+                    <h1 className="heading">Book Appointment with Trusted Doctors</h1>
+                    <div className="subpart"> 
+                        <img src={assets.group_profiles} className="home_group_img" alt='home_left_group_image'/>
+                        <p className="subheading">Simply Browse through the extensive list of trusted doctors. Schedule you appointment hasiie free</p> 
+                        </div> 
+                        <button className="home_main_btn" onClick={()=> navigate('/appointment')}>Book Appointment Now</button> 
+                        </div> 
+                        <img src={assets.header_img} alt="homepage_main_image" className="main_img"/> 
+                        </div> 
+                        <section className="speciality">
+                            <div className="speciality_wrapper">
+                            <h3 className="speciality_heading">Find by Speciality</h3> 
+                            <p className="speciality_subheading">Simply browse through our extensive list ot trusted doctors, shedule your appointment free </p> 
+                            <div className="main_speciality">
+                                {
+                                 specialityData.map((special,index)=>(
+                                    <div className="speciality_img_heading" key={index}>
+                                    <img src={special.image} alt='speciality_image' className="speciality_image"/>
+                                     <span className="special_heading">{special.speciality}</span> </div> )) 
+                                     }
+                                      
+                                      </div>
+                                       </div>
+                                    </section>
+                                    <section className="doctors_view">
+                                         </section> 
+            </div>
+            </div> 
+            ) 
+    }; 
 export default Home;
